df548c20cb9858065206ddb2c2e5d41bdfd7fb94
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-08-07 17:05:04 -0500

    analyze
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- apps/writing-next/next.config.js
+++ apps/writing-next/next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -11,4 +15,4 @@ const nextConfig = {
   transpilePackages: ['@libs', '@marbemac'],
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
