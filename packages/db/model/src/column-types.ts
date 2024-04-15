34e5d91221db6d1d4fd56a03fee651d5f720b065
Marc MacLeod <marbemac+gh@gmail.com>
2024-04-15 17:15:57 -0500

    add timestamp64 helper
    Signed-off-by: Marc MacLeod <marbemac+gh@gmail.com>

--- packages/db/model/src/column-types.ts
+++ packages/db/model/src/column-types.ts
@@ -25,3 +25,13 @@ export const idCol = <Type extends string>() => {
  */
 export const timestampCol = <TName extends string>(dbName: TName) =>
   timestamp(dbName, { withTimezone: true, mode: 'date' });
+
+/**
+ * @example
+ * const columns = {
+ *   createdAt: timestampCol('created_at').notNull().defaultNow(),
+ *   updatedAt: timestampCol('updated_at').defaultNow(),
+ * }
+ */
+export const timestamp64Col = <TName extends string>(dbName: TName) =>
+  timestamp(dbName, { withTimezone: true, mode: 'date', precision: 6 });
