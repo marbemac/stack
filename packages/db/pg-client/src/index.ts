e5357c09434afc3694cedc055ba44a2ddb7f6135
Marc MacLeod <marbemac+gh@gmail.com>
2024-05-15 15:32:09 -0500

    lint
    Signed-off-by: Marc MacLeod <marbemac+gh@gmail.com>

--- packages/db/pg-client/src/index.ts
+++ packages/db/pg-client/src/index.ts
@@ -1,3 +1,3 @@
 export { initClient } from './init-client.ts';
-export type { InitClientOpts, PgClientMetrics } from './types.ts';
 export { safeBulkOp } from './safe-bulk.ts';
+export type { InitClientOpts, PgClientMetrics } from './types.ts';
